d48f26590e3ac6f6c80bde1beffeac6ffa03f277
Sandeep Jadoonanan <jsanweb@gmail.com>
2016-05-30 04:34:59 -0400

    Remove unnecessary imports.

--- app/profiles/views.py
+++ app/profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponse
 from django.contrib.auth.views import login, logout
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
